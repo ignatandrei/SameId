17a78fbb35d27b819ce0eb8ea512565ad095cf95
Andrei Ignat <ignatandrei@yahoo.com>
2013-08-30 08:07:08 +0300

    versio

---
+++ SameId/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SameId.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public static Product GetFromId(int id)
+        {
+            return new Product() { Id = id, Name = " product number :" + id };
+        }
+    }
+}
