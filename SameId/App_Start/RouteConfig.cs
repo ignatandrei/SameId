d6de6608ed42725a231fd9519273112496deeaf8
Andrei Ignat <ignatandrei@yahoo.com>
2013-08-31 20:18:32 +0300

    now in separate dll

--- SameId/App_Start/RouteConfig.cs
+++ SameId/App_Start/RouteConfig.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using SameIdAlert;
 
 namespace SameId
 {
@@ -12,7 +15,8 @@ namespace SameId
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapHubs();
+            routes.MapHubs();            
+            GlobalHost.DependencyResolver.Register(typeof(IAssemblyLocator) ,() => new SameIdAssemblyLocator());
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ SameIdAlert/AssemblyLocatorHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SameIdAlert
+{
+    public class SameIdAssemblyLocator: IAssemblyLocator
+    {
+        public IList<Assembly> GetAssemblies()
+        {
+            return new List<Assembly>() { Assembly.GetCallingAssembly(), Assembly.GetExecutingAssembly() };
+        }
+    }
+}
